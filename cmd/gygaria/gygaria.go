3af7d40a55c3c39bca6910035ed6df75a89f623c
Brandon Locker <brandonlocker@hotmail.com>
2020-03-23 13:03:45 -0500

    more structuring

---
+++ cmd/gygaria/gygaria.go
@@ -0,0 +1,10 @@
+// Package gygaria is a package to pre-install the Gygaria TBA (Text Based Adventure). 
+// By pre-install means creating the json file for the "Unnamed" TBA engine.
+// Will also act as a test bed for the TBA engine's functionality
+package gygaria
+
+func creatGygaria() {
+	// code to pre-fill in all the map cells
+
+	
+}
